14797d9c3f75739c10de66989b57177c9f59e67d
EchoHugh1124 <echoxiongyue@163.com>
2021-03-17 17:20:10 +0100

    Initial commit

---
+++ R/hello.R
@@ -0,0 +1,16 @@
+#' My hello world function
+#'
+#'
+#' @param x The name of the person to say hi to.
+#'
+#' @return The output from \code{\link{print}}
+#' @export
+#'
+#' @examples
+#' hello("John")
+#' \dontrun{
+#' hello("Steve")
+#' }
+hello <- function(x) {
+  print(x)
+}
